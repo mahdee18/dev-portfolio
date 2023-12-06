4ff3650d45b73aad7b3f4a2e1360ccc85660cebd
mahdee18 <mahdeec10@gmail.com>
2023-12-06 13:02:34 +0600

    Fix some issues

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -18,137 +18,155 @@ import img3 from '../assets/Thai1.png';
 import img4 from '../assets/NexusBank.png';
 import img5 from '../assets/RoyalRent.png';
 export const content = {
-  skills: [
-    {
-        name: "React.js",
-        para: "JavaScript library for building user interfaces with component-based architecture.",
-        logo: reactjs,
-        type: "frontend"
-    },
-    {
-        name: "Node.js",
-        para: "Runtime environment for server-side JavaScript execution.",
-        logo: nodejs,
-        type: "backend"
-    },
-    {
-        name: "Express.js",
-        para: "Minimalistic and flexible web application framework for Node.js.",
-        logo: expressjs,
-        type: "backend"
-    },
-    {
-        name: "MongoDB",
-        para: "Scalable and flexible NoSQL database storing data in JSON.",
-        logo: mongodb,
-        type: "backend"
-    },
-    {
-        name: "HTML5",
-        para: "Standard markup language for creating web page structure and content.",
-        logo: html,
-        type: "frontend"
-    },
-    {
-        name: "CSS3",
-        para: "Styling language for describing the presentation of HTML elements.",
-        logo: css,
-        type: "frontend"
-    },
-    {
-        name: "Tailwind CSS",
-        para: "Utility-first CSS framework providing pre-built classes.",
-        logo: tailwind,
-        type: "frontend"
-    },
-    {
-        name: "Bootstrap",
-        para: "Popular front-end framework with pre-designed components.",
-        logo: bootstrap,
-        type: "frontend"
-    },
-    {
-        name: "Firebase",
-        para: "Comprehensive backend-as-a-service platform by Google.",
-        logo: firebase,
-        type: "backend"
-    },
-    {
-        name: "VS Code",
-        para: "Feature-rich source code editor supporting multiple programming languages.",
-        logo: vscode,
-        type: "others"
-    },
-    {
-        name: "Figma",
-        para: "Collaborative and cloud-based design tool empowering teams.",
-        logo: figma,
-        type: "others"
-    },
-    {
-        name: "Canva",
-        para: "User-friendly graphic design platform for creating professional designs.",
-        logo: canva,
-        type: "others"
-    },
-    {
-        name: "GitHub",
-        para: "Streamline collaboration and elevate productivity with robust version control.",
-        logo: "https://www.pardcode.com/Media/opengl3d_4.png",
-        type: "others"
-    },
-    {
-        name: "Redux",
-        para: "Effortless state management enhancing JavaScript apps with simplicity and scalability.",
-        logo: "https://icons.veryicon.com/png/o/education-technology/big-data-1/dide.png",
-        type: "frontend"
-    },
-    {
-        name: "Netlify",
-        para: "Streamlined hosting and effortless deployment for seamless web experiences.",
-        logo: "https://static-00.iconduck.com/assets.00/netlify-icon-511x512-idkvcd89.png",
-        type: "others"
-    },
-    {
-        name: "Vercel",
-        para: "Swift deployments, effortless scaling. Instant previews, serverless functions. Elevate projects with unmatched speed.",
-        logo: "https://www.buitengewoonanders.be/wp-content/uploads/2017/04/Black_triangle2.svg_.png",
-        type: "others"
-    }
-],
+    skills: [
+        {
+            name: "React.js",
+            para: "JavaScript library for building user interfaces with component-based architecture.",
+            logo: reactjs,
+            type: "frontend"
+        },
+        {
+            name: "Node.js",
+            para: "Runtime environment for server-side JavaScript execution.",
+            logo: nodejs,
+            type: "backend"
+        },
+        {
+            name: "Express.js",
+            para: "Minimalistic and flexible web application framework for Node.js.",
+            logo: expressjs,
+            type: "backend"
+        },
+        {
+            name: "MongoDB",
+            para: "Scalable and flexible NoSQL database storing data in JSON.",
+            logo: mongodb,
+            type: "backend"
+        },
+        {
+            name: "HTML5",
+            para: "Standard markup language for creating web page structure and content.",
+            logo: html,
+            type: "frontend"
+        },
+        {
+            name: "CSS3",
+            para: "Styling language for describing the presentation of HTML elements.",
+            logo: css,
+            type: "frontend"
+        },
+        {
+            name: "Tailwind CSS",
+            para: "Utility-first CSS framework providing pre-built classes.",
+            logo: tailwind,
+            type: "frontend"
+        },
+        {
+            name: "Bootstrap",
+            para: "Popular front-end framework with pre-designed components.",
+            logo: bootstrap,
+            type: "frontend"
+        },
+        {
+            name: "Firebase",
+            para: "Comprehensive backend-as-a-service platform by Google.",
+            logo: firebase,
+            type: "backend"
+        },
+        {
+            name: "VS Code",
+            para: "Feature-rich source code editor supporting multiple programming languages.",
+            logo: vscode,
+            type: "others"
+        },
+        {
+            name: "Figma",
+            para: "Collaborative and cloud-based design tool empowering teams.",
+            logo: figma,
+            type: "others"
+        },
+        {
+            name: "Canva",
+            para: "User-friendly graphic design platform for creating professional designs.",
+            logo: canva,
+            type: "others"
+        },
+        {
+            name: "GitHub",
+            para: "Streamline collaboration and elevate productivity with robust version control.",
+            logo: "https://www.pardcode.com/Media/opengl3d_4.png",
+            type: "others"
+        },
+        {
+            name: "Redux",
+            para: "Effortless state management enhancing JavaScript apps with simplicity and scalability.",
+            logo: "https://icons.veryicon.com/png/o/education-technology/big-data-1/dide.png",
+            type: "frontend"
+        },
+        {
+            name: "Netlify",
+            para: "Streamlined hosting and effortless deployment for seamless web experiences.",
+            logo: "https://static-00.iconduck.com/assets.00/netlify-icon-511x512-idkvcd89.png",
+            type: "others"
+        },
+        {
+            name: "Vercel",
+            para: "Swift deployments, effortless scaling. Instant previews, serverless functions. Elevate projects with unmatched speed.",
+            logo: "https://www.buitengewoonanders.be/wp-content/uploads/2017/04/Black_triangle2.svg_.png",
+            type: "others"
+        }
+    ],
 
 
- projects: [
-    {
-      "title": "Nexus Bank Ltd.",
-      "description": "Developed a website focused on Digital Banking Web Application.",
-      "previewLink": "https://nexus-cc1a9.web.app/",
-      "imageSrc": img4
-    },
-    {
-      "title": "Capture Academy",
-      "description": "Developed a website focused on photography education and summer camp.",
-      "previewLink": "https://capture-academy-1a91f.web.app/",
-      "imageSrc": img1
-    },
-    {
-      "title": "Smart Toy Market",
-      "description": "Create an engaging platform for purchasing educational toys.",
-      "previewLink": "https://smarttoymarket.web.app/",
-      "imageSrc": img2
-    },
-    {
-      "title": "Thai Kitchen",
-      "description": "Create an exclusive online platform dedicated to Thai cuisine.",
-      "previewLink": "https://thai-kitchen-d45d7.web.app/",
-      "imageSrc": img3
-    },
-    {
-      "title": "Royal Rent",
-      "description": "Create an exclusive online platform dedicated to Digital Car Rental Web App.",
-      "previewLink": "https://royal-rent-client.vercel.app/",
-      "imageSrc": img5
-    }
-  ]
+    projects: [
+        {
+            "title": "Nexus Bank Ltd.",
+            "description": "Developed a website focused on Digital Banking Web Application.",
+            "client": "https://github.com/codewithsiam/nexus-bank-ltd-client",
+            "server": "https://github.com/codewithsiam/nexus-bank-ltd-server",
+            "live_link": "https://nexus-cc1a9.web.app/",
+            "imageSrc": img4
+        },
+        {
+            "title": "Capture Academy",
+            "description": "Developed a website focused on photography education and summer camp.",
+            "live_link": "https://capture-academy-1a91f.web.app/",
+            "client": "https://github.com/mahdee18/capture-academy-client",
+            "server": "https://github.com/mahdee18/capture-academy-server",
+            "imageSrc": img1
+        },
+        {
+            "title": "Smart Toy Market",
+            "description": "Create an engaging platform for purchasing educational toys.",
+            "live_link": "https://smarttoymarket.web.app/",
+            "client": "https://github.com/mahdee18/smart-toy-market-client",
+            "server": "https://github.com/mahdee18/smart-toy-market-server",
+            "imageSrc": img2
+        },
+        {
+            "title": "Thai Kitchen",
+            "description": "Create an exclusive online platform dedicated to Thai cuisine.",
+            "live_link": "https://thai-kitchen-d45d7.web.app/",
+            "client": "https://github.com/mahdee18/thai-kitchen-client",
+            "server": "https://github.com/mahdee18/thai-kitchen-server",
+            "imageSrc": img3
+        },
+        {
+            "title": "Royal Rent",
+            "description": "Create an exclusive online platform dedicated to Digital Car Rental Web App.",
+            "live_link": "https://royal-rent-client.vercel.app/",
+            "client": "https://github.com/mutanim24/royal-rent-client",
+            "server": "https://github.com/mutanim24/royal-rent-server",
+            "imageSrc": img5
+        },
+        {
+            "title": "Royal Rent",
+            "description": "Create an exclusive online platform dedicated to Digital Car Rental Web App.",
+            "live_link": "https://royal-rent-client.vercel.app/",
+            "client": "https://github.com/mutanim24/royal-rent-client",
+            "server": "https://github.com/mutanim24/royal-rent-server",
+            "imageSrc": img5
+        }
+    ]
 
 };
--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -1,19 +1,17 @@
 import React from "react"
 
-export default function ProjectCard({project}) {
-    console.log(project)
-    const {imageSrc,title,description,previewLink} = project
+export default function ProjectCard({ project }) {
+  console.log(project)
+  const { imageSrc, title, description, live_link, client, server } = project
   return (
     <>
-      <div className="overflow-hidden rounded text-slate-500 shadow-md shadow-slate-200">
+      <div className="overflow-hidden rounded text-slate-500 shadow-lg shadow-slate-200 group relative">
         {/*  <!-- Image --> */}
-        <figure>
-          <img
-            src={imageSrc}
-            alt="card image"
-            className="aspect-video w-full"
-          />
-        </figure>
+        <img
+          src={imageSrc}
+          alt="card image"
+          className="aspect-video w-full"
+        />
         {/*  <!-- Body--> */}
         <div className="p-6">
           <header className="mb-4">
@@ -25,13 +23,31 @@ export default function ProjectCard({project}) {
             {description}
           </p>
         </div>
+        {/* Text overlay */}
+        <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-80 duration-500 flex items-center justify-center transform translate-y-full group-hover:translate-y-0 transition-transform">
+          <div className="text-white text-left px-4">
+            <h2 className="text-3xl font-bold mb-3">{title}</h2>
+            <p className="my-3 text-base text-white font-thin">
+              {description}
+            </p>
+            <div className="flex justify-between py-4 gap-2">
+              <a className="btn w-1/2 py-3 px-2 bg-accent text-white font-bold" href={client}>Client</a>
+              <a href={server} className="btn w-1/2 py-3 px-2 bg-accent text-white font-bold">Server</a>
+            </div>
+            <a href={live_link} target="_blank" className="inline-flex btn h-10 w-full items-center justify-center gap-2 text-white disabled:shadow-none rounded-none">
+              <span>View Live</span>
+            </a>
+          </div>
+        </div>
         {/*  <!-- Action base sized basic button --> */}
-        <div className="flex justify-end p-6 pt-0">
-          <a href={previewLink} target="_blank" className="inline-flex btn h-10 w-full items-center justify-center gap-2 whitespace-nowrap rounded bg-emerald-500 px-5 text-sm font-medium tracking-wide text-white transition duration-300 hover:bg-emerald-600 focus:bg-emerald-700 focus-visible:outline-none disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none">
+        {/* <div className="flex justify-end p-6 pt-0">
+          <a href={live_link} target="_blank" className="inline-flex btn h-10 w-full items-center justify-center gap-2 text-white disabled:shadow-none rounded-none">
             <span>View Details</span>
           </a>
-        </div>
+        </div> */}
       </div>
+
+
     </>
   )
 }
